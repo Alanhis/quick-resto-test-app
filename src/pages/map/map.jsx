8dbd5091392174eb6c096dc54034b3f865cbd72a
Alan Khismatullin <alanhis043@gmail.com>
2023-06-02 15:24:38 +0300

    Настройка отображения карты

--- src/pages/map/map.jsx
+++ src/pages/map/map.jsx
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { CookieCheck } from "../../utils/cookie-check";
-
+import { Map } from "react-canvas-map";
+import { ReactDOM } from "react";
+import TestSvg from "../../utils/tutzing.svg";
 export function MapPage() {
   const location = useLocation().pathname;
   const navigate = useNavigate();
   useEffect(() => {
     CookieCheck({ navigate, location });
   });
-  return <div></div>;
+  return (
+    <div>
+      <img src={TestSvg} alt="logo" />
+    </div>
+  );
 }
