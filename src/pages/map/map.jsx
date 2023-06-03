b501a4f0ffe39c6d0157898757ad632c2bae2e54
Alan Khismatullin <alanhis043@gmail.com>
2023-06-03 21:40:47 +0300

    формирование отображение точек из json файла

--- src/pages/map/map.jsx
+++ src/pages/map/map.jsx
@@ -1,18 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { CookieCheck } from "../../utils/cookie-check";
-import { Map } from "react-canvas-map";
+import TestData from "../../utils/model.json";
 import { ReactDOM } from "react";
 import TestSvg from "../../utils/tutzing.svg";
+import ImageMarker from "react-image-marker";
+import "./map-style.css";
 export function MapPage() {
   const location = useLocation().pathname;
   const navigate = useNavigate();
+  let [markers, setMarkers] = useState([]);
   useEffect(() => {
     CookieCheck({ navigate, location });
-  });
+  }); // Проверка закончился период доступа
+  useEffect(() => {
+    TestData.forEach((data) => {
+      console.log(data);
+      setMarkers((oldData) => [
+        ...oldData,
+        {
+          top: data.x,
+          left: data.y - 3,
+          textMark: data.name,
+          amount: data.amount,
+        },
+      ]);
+    });
+  }, []); // Подгрузка первичных элементов
+  const CustomMarker = (props) => {
+    return (
+      <div className="marker" style={{ display: "flex" }}>
+        <div
+          className="image-marker__marker image-marker__marker--default "
+          data-testid="marker"
+        >
+          {/* {props.itemNumber} */}
+        </div>
+
+        <div className="text" style={{ color: "black", marginLeft: "2rem" }}>
+          {props?.textMark} x{props?.amount}
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <img src={TestSvg} alt="logo" />
+    <div className="map-container">
+      <ImageMarker
+        src={TestSvg}
+        markers={markers}
+        onAddMarker={(marker) => setMarkers((prev) => [...prev, marker])}
+        markerComponent={CustomMarker}
+      />
     </div>
   );
 }
